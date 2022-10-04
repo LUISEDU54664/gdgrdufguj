af453d0cb24dea5903311ed32a8270ffcd6a2e9b
LUISEDU54664 <111461721+LUISEDU54664@users.noreply.github.com>
2022-10-04 12:00:01 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (total + 1))
+})
+let distancia = 0
 let total = 0
-if (sonar.ping(
-DigitalPin.P1,
-DigitalPin.P1,
-PingUnit.Centimeters
-) == total) {
-	
+if (total == 0) {
+    distancia = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(total)
 }
-basic.showNumber(total)
 basic.forever(function () {
 	
 })
